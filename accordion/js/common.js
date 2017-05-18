33f421589d1337b21a08c96d33d14d90e11aa5b9
Eugene Abrosimenkov <eabrosimenkov@gmail.com>
2017-05-18 13:18:47 +0200

    accordion

---
+++ accordion/js/common.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+	$('.accordion-title + .accordion-text').not(':first').hide();
+
+	$('.accordion-title').click(function() {
+		var accordionText = $(this).next('.accordion-text'),
+		arrow = $(this).children('.arrow');
+		accordion = $(this).closest('.accordion'),
+		titleIndex = $(this).index();
+		//arrow = $('.arrow');
+		img = $('.service-img');
+		
+		if (accordionText.is(':visible')) {
+			accordionText.slideUp();
+			arrow.toggleClass('transformed');
+		} else {
+			accordion.find('.accordion-text').slideUp();
+			accordionText.slideDown();
+			arrow.toggleClass('transformed');
+		}
+
+		if (titleIndex === 0) {
+			$(img[0]).addClass('active').siblings().removeClass('active');
+		} else if (titleIndex === 2) {
+			$(img[1]).addClass('active').siblings().removeClass('active');
+		} else if (titleIndex === 4) {
+			$(img[2]).addClass('active').siblings().removeClass('active');
+		}
+
+	});
+	
+});
